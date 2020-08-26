417f10ec4b972c8cd55fb4c07791acd7cfce9e5b
Julio Arruda <julio.arruda@outlook.com>
2020-08-25 22:52:56 -0300

    Commit Inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div>Olá, seja muito bem vindo a apresentação de GitHub Actions do TDC, na sala Microsoft</div>`,
+})
+export class AppComponent {
+  value = 'World';
+}
